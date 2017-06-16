6803bc3842284b0ae0700e8aff7a630494bbfa45
BridgeNB <zyq123258789@gmail.com>
2017-06-15 23:22:20 -0400

    Landscale layout issue solved

--- Counter/Counter/ViewController.swift
+++ Counter/Counter/ViewController.swift
@@ -15,14 +15,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // label
-        var label = UILabel()
+        let label = UILabel()
         label.frame = CGRect.init(x: 150, y: 150, width: 60, height:60)
         label.text = "0"
         self.view.addSubview(label)
         self.label = label
         
         // Increment Button
-        var button = UIButton()
+        let button = UIButton()
         button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
         button.setTitle("Click", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
@@ -31,9 +31,28 @@ class ViewController: UIViewController {
         button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
         
         // Decrement Button
+        let decre_button = UIButton()
+        decre_button.frame = CGRect.init(x: 150, y: 350, width: 60, height: 60)
+        decre_button.setTitle("Decrement click!", for: .normal)
+        decre_button.setTitleColor(UIColor.red, for: .normal)
+        self.view.addSubview(decre_button)
+        // Class learned
+        // ***************
+        // Actions are method indications from views back to view controllers
+        // ***************
+        decre_button.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControlEvents.touchUpInside)
     }
+    // Callback function
     func incrementCount() {
         self.count += 1
+        self.label.text = "\(self.count)"
+    }
+    func decrementCount() {
+        self.count -= 1
+        // Class learned
+        // ********************
+        // Outlets are allowing method indication from controller to view
+        // ********************
         self.label.text = "\(self.count)"
     }
 }
